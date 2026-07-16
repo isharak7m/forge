ce9f20473d45c5533717d5b26a22cadf10fe041a
isharak7m <192635824+isharak7m@users.noreply.github.com>
2026-07-16 11:09:58 +0530

    Add V5 migration to fix food_items column names (per_100g -> per100g)

---
+++ src/main/resources/db/migration/V5__fix_food_items_column_names.sql
@@ -0,0 +1,5 @@
+ALTER TABLE food_items RENAME COLUMN calories_per_100g TO calories_per100g;
+ALTER TABLE food_items RENAME COLUMN protein_per_100g TO protein_per100g;
+ALTER TABLE food_items RENAME COLUMN carbs_per_100g TO carbs_per100g;
+ALTER TABLE food_items RENAME COLUMN fat_per_100g TO fat_per100g;
+ALTER TABLE food_items RENAME COLUMN fiber_per_100g TO fiber_per100g;
